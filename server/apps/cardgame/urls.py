a00c2f14b4647e79d254a03fb2e77b3bc7e56c7a
gardening-y <60008786@naver.com>
2023-01-19 23:34:41 +0900

    [feat] url 생성

--- server/apps/cardgame/urls.py
+++ server/apps/cardgame/urls.py
@@ -4,5 +4,11 @@ from . import views
 app_name = "cardgame"
 
 urlpatterns = [
-  path("", views.base, name="base"),
+  path("", views.main, name="main"),
+  path("game/", views.game_list, name="list"),
+  path("game/rank/", views.game_rank, name="rank"),
+  path('game/create/', views.game_create, name='create'),
+  path("game/<int:pk>/", views.main, name="detail"),
+  path('game/receive/<int:pk>/', views.game_receive, name='update'),
+  # path('game/delete/<int:pk>/', views.game_delete, name='delete'),
 ]
