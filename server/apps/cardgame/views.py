30f5de072174a4856e88759f0a945301eff0f5f8
gardening-y <60008786@naver.com>
2023-01-19 23:34:24 +0900

    [test] html 연동을 위한 임시 views 구현

--- server/apps/cardgame/views.py
+++ server/apps/cardgame/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
-def base(request, *args, **kwargs):
-  return render(request, "cardgame/base.html")
+def main(request, *args, **kwargs):
+  return render(request, "cardgame/main.html")
+
+def game_list(request, *args, **kwargs):
+  return render(request, "cardgame/game_list.html")
+
+def game_detail(request, *args, **kwargs):
+  return render(request, "cardgame/game_detail.html")
+
+def game_create(request, *args, **kwargs):
+  return render(request, "cardgame/game_create.html")
+
+def game_receive(request, *args, **kwargs):
+  return render(request, "cardgame/game_receive.html")
+
+def game_rank(request, *args, **kwargs):
+  return render(request, "cardgame/ranking.html")
+
+# def game_delete(request, *args, **kwargs):
+#   return redirect('')
+
